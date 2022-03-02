c7abb6445b8d256b9d0eeaee25e7b0b51fc43653
gowtham758550 <gowtham758550@gmail.com>
2022-03-02 20:47:43 +0530

    buttons added in employee module, add employee module created with reactive forms

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-employees',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
+  employeeButtonMenu = [
+    {value: 'All Employees'},
+    {value: 'Active Employees'},
+    {value: 'Incomplete Employees'},
+    {value: 'Portal Enabled Employees'},
+    {value: 'Portal Disabled Employees'},
+    {value: 'Terminated Employees'},
+  ]
+  employeeButtonValue: string = 'Active Employees'
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.router.url)
   }
 
 }
